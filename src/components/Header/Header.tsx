2c63fbef8b56df15e7b1695fe971e0a20739a694
S4mm4el <homer1996@gmail.com>
2022-11-18 16:00:34 +0300

    feat: add header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
+import logo from '../../assets/img/Logo.png';
+import langchange from '../../assets/img/Langchange.png';
 
 function Header() {
   return (
     <>
       <header>
-        <div className="header__wrapper">
-          <nav>
-            <Link to="/login">
-              <button>Sign In</button>
-            </Link>
-            <Link to="/register">
-              <button>Sign Up</button>
-            </Link>
-          </nav>
+        <div className={styles.header__wrapper}>
+          <div className={styles.header__logo}>
+            <img src={logo} alt="logo" />
+          </div>
+          <div className={styles.nav__wrapper}>
+            <img className={styles.lang} src={langchange} alt="choose lang" />
+            <nav>
+              <Link to="/login">
+                <button className={styles.sign__in}>Sign In</button>
+              </Link>
+              <Link to="/register">
+                <button className={styles.sign__up}>Sign Up</button>
+              </Link>
+            </nav>
+          </div>
         </div>
       </header>
     </>
