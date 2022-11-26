9ce3edee72760999a191d3de5c13a247d0d74b8d
Sammily <sammily03@yandex.ru>
2022-11-26 19:22:04 +0300

    feat: add i18next

--- src/i18n.js
+++ src/i18n.js
@@ -39,3 +39,28 @@ i18n.use(initReactI18next).init({
 });
 
 export default i18n;
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+const resources = {
+  en: {
+    translation: {
+      'Log out': 'Log out',
+    },
+  },
+  ru: {
+    translation: {
+      'Log out': 'Выйти',
+    },
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
