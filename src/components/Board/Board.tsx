989d0ee4e270249441618dc3d7d0cd47d1b2fe99
S4mm4el <homer1996@gmail.com>
2022-11-29 15:45:59 +0300

    feat: add back button to board

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,31 +1,39 @@
 import {} from '@chakra-ui/icons';
 import { ChakraProvider, Container, SimpleGrid, theme } from '@chakra-ui/react';
 import { DndProvider } from 'react-dnd';
+import { Link } from 'react-router-dom';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import Column from '../Column/Column';
 import { ColumnType } from '../../utils/enums';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import styles from './Board.module.css';
 
 function Board() {
   return (
-    <ChakraProvider theme={theme}>
-      <Header />
-      <section className="board__section">
-        <DndProvider backend={HTML5Backend}>
-          <Container maxWidth="container.lg" px={4} py={14}>
-            <SimpleGrid columns={{ base: 1, md: 4 }} spacing={{ base: 16, md: 4 }}>
-              <Column column={ColumnType.TO_DO} />
-              <Column column={ColumnType.IN_PROGRESS} />
-              <Column column={ColumnType.BLOCKED} />
-              <Column column={ColumnType.COMPLETED} />
-            </SimpleGrid>
-          </Container>
-        </DndProvider>
-      </section>
+    <>
+      <ChakraProvider theme={theme}>
+        <Header />
 
-      <Footer />
-    </ChakraProvider>
+        <section className={styles.board__section}>
+          <Link to="/">
+            <button className={styles.back__button}>Main</button>
+          </Link>
+          <DndProvider backend={HTML5Backend}>
+            <Container maxWidth="container.lg" px={4} py={8}>
+              <SimpleGrid columns={{ base: 1, md: 4 }} spacing={{ base: 16, md: 4 }}>
+                <Column column={ColumnType.TO_DO} />
+                <Column column={ColumnType.IN_PROGRESS} />
+                <Column column={ColumnType.BLOCKED} />
+                <Column column={ColumnType.COMPLETED} />
+              </SimpleGrid>
+            </Container>
+          </DndProvider>
+        </section>
+
+        <Footer />
+      </ChakraProvider>
+    </>
   );
 }
 
