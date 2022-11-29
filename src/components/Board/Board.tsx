a5eb1893c6ca20e498903f80ad44ef51a9dc19a0
S4mm4el <homer1996@gmail.com>
2022-11-29 09:19:23 +0300

    refactor: component Board rendering position

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,25 +1,32 @@
 import {} from '@chakra-ui/icons';
-import { Container, Heading, SimpleGrid } from '@chakra-ui/react';
+import { ChakraProvider, Container, SimpleGrid, theme } from '@chakra-ui/react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import Column from '../Column/Column';
 import { ColumnType } from '../../utils/enums';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
-function App() {
+function Board() {
   return (
-    <main>
-      <DndProvider backend={HTML5Backend}>
-        <Container maxWidth="container.lg" px={4} py={14}>
-          <SimpleGrid columns={{ base: 1, md: 4 }} spacing={{ base: 16, md: 4 }}>
-            <Column column={ColumnType.TO_DO} />
-            <Column column={ColumnType.IN_PROGRESS} />
-            <Column column={ColumnType.BLOCKED} />
-            <Column column={ColumnType.COMPLETED} />
-          </SimpleGrid>
-        </Container>
-      </DndProvider>
-    </main>
+    <ChakraProvider theme={theme}>
+      <Header />
+      <section className="board__section">
+        <DndProvider backend={HTML5Backend}>
+          <Container maxWidth="container.lg" px={4} py={14}>
+            <SimpleGrid columns={{ base: 1, md: 4 }} spacing={{ base: 16, md: 4 }}>
+              <Column column={ColumnType.TO_DO} />
+              <Column column={ColumnType.IN_PROGRESS} />
+              <Column column={ColumnType.BLOCKED} />
+              <Column column={ColumnType.COMPLETED} />
+            </SimpleGrid>
+          </Container>
+        </DndProvider>
+      </section>
+
+      <Footer />
+    </ChakraProvider>
   );
 }
 
-export default App;
+export default Board;
--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,19 +1,10 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import theme from '../../config/theme';
-import Boards from '../Board/Board';
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
+import Board from '../Board/Board';
 
 function MainPage() {
   return (
-    <ChakraProvider theme={theme}>
-      <Header />
-
-      <section className="boards__section">
-        <Boards />
-      </section>
-      <Footer />
-    </ChakraProvider>
+    <section className="main__section">
+      <Board />
+    </section>
   );
 }
 
