9db1d859616a242720031d2e3d83e806c4307bda
Semion Krapivin <homer1996@gmail.com>
2022-12-01 23:54:43 +0300

    fix: return button to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -73,6 +73,9 @@ function Header() {
                 <Link to="/profile">
                   <button className={styles.sign__in}>{t('Edit Profile')}</button>
                 </Link>
+                <button className={styles.sign__out} onClick={handleLogOut}>
+                  {t('Sign Out')}
+                </button>
               </nav>
             )}
           </div>
